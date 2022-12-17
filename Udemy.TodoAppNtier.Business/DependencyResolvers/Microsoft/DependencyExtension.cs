6e9eb843c5bc017b117bdd31bb46508ff1f9e343
Emirhan <102459178+emrhnclngr@users.noreply.github.com>
2022-12-17 23:55:32 +0300

    Add project files.

---
+++ Udemy.TodoAppNtier.Business/DependencyResolvers/Microsoft/DependencyExtension.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Udemy.TodoAppNTier.Business.Interfaces;
+using Udemy.TodoAppNTier.Business.Mappings.AutoMapper;
+using Udemy.TodoAppNTier.Business.Services;
+using Udemy.TodoAppNTier.Business.ValidationRules;
+using Udemy.TodoAppNTier.DataAccess.Contexts;
+using Udemy.TodoAppNTier.DataAccess.UnitofWork;
+using Udemy.TodoAppNTier.Dtos.WorkDtos;
+
+namespace Udemy.TodoAppNTier.Business.DependencyResolvers.Microsoft
+{
+    public static class DependencyExtension
+    {
+        public static void AddDependencies(this IServiceCollection services)
+        {
+
+            services.AddDbContext<TodoContext>(opt =>
+            {
+                opt.UseSqlServer("server=.; database=TodoDb;Trusted_Connection=True;");
+                opt.LogTo(Console.WriteLine, LogLevel.Information);
+            });
+            var configuration = new MapperConfiguration(opt =>
+            {
+                opt.AddProfile(new WorkProfile());
+            });
+            var mapper = configuration.CreateMapper();
+
+            services.AddSingleton(mapper);
+
+            services.AddScoped<IUow, Uow>();
+            services.AddScoped<IWorkService, WorkService>();
+
+            services.AddTransient<IValidator<WorkCreateDto>, WorkCreateDtoValidator>();
+            services.AddTransient<IValidator<WorkUpdateDto>, WorkUpdateDtoValidator>();
+        }
+    }
+}
---
+++ Udemy.TodoAppNtier.Business/Services/WorkService.cs
@@ -0,0 +1,104 @@
+﻿using AutoMapper;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Udemy.TodoAppNTier.Business.Extensions;
+using Udemy.TodoAppNTier.Business.Interfaces;
+using Udemy.TodoAppNTier.Business.ValidationRules;
+using Udemy.TodoAppNTier.Common.ResponseObjects;
+using Udemy.TodoAppNTier.DataAccess.UnitofWork;
+using Udemy.TodoAppNTier.Dtos.Interfaces;
+using Udemy.TodoAppNTier.Dtos.WorkDtos;
+using Udemy.TodoAppNTier.Entities.Domains;
+
+namespace Udemy.TodoAppNTier.Business.Services
+{
+    public class WorkService : IWorkService
+    {
+        private readonly IUow _uow;
+        private readonly IMapper _mapper;
+        private readonly IValidator<WorkCreateDto> _createDtoValidator;
+        private readonly IValidator<WorkUpdateDto> _updateDtoValidator;
+
+        public WorkService(IUow uow, IMapper mapper, IValidator<WorkCreateDto> createDtoValidator, IValidator<WorkUpdateDto> updateDtoValidator)
+        {
+            _uow = uow;
+            _mapper = mapper;
+            _createDtoValidator = createDtoValidator;
+            _updateDtoValidator = updateDtoValidator;
+        }
+
+        public async Task<IResponse<WorkCreateDto>> Create(WorkCreateDto dto)
+        {
+            var validationResult = _createDtoValidator.Validate(dto);
+
+
+            if (validationResult.IsValid)
+            {
+                await _uow.GetRepository<Work>().Create(_mapper.Map<Work>(dto));
+
+                await _uow.SaveChanges();
+                return new Response<WorkCreateDto>(ResponseType.Success, dto);
+            }
+            else
+            {
+                return new Response<WorkCreateDto>(ResponseType.ValidationError, dto, validationResult.ConvertToCustomValidationError());
+            }
+
+
+        }
+
+        public async Task<IResponse<List<WorkListDto>>> GetAll()
+        {
+
+            var data = _mapper.Map<List<WorkListDto>>(await _uow.GetRepository<Work>().GetAll());
+            return new Response<List<WorkListDto>>(ResponseType.Success, data);
+        }
+
+        public async Task<IResponse<IDto>> GetById<IDto>(int id)
+        {
+           var data = _mapper.Map<IDto>(await _uow.GetRepository<Work>().GetByFilter(x => x.Id == id));
+            if(data ==null)
+            {
+                return new Response<IDto>(ResponseType.NotFound, $"Data for {id} not found!");
+            }
+            return new Response<IDto>(ResponseType.Success,data);
+        }
+
+        public async Task<IResponse> Remove(int id)
+        {
+            var removedEntity = await _uow.GetRepository<Work>().GetByFilter(x => x.Id == id);
+            if(removedEntity != null)
+            {
+                _uow.GetRepository<Work>().Remove(removedEntity);
+                await _uow.SaveChanges();
+                return new Response(ResponseType.Success);
+            }
+            return new Response(ResponseType.NotFound, $"Data for {id} not found!");
+        }
+
+        public async Task<IResponse<WorkUpdateDto>> Update(WorkUpdateDto dto)
+        {
+            var result = _updateDtoValidator.Validate(dto);
+            if (result.IsValid)
+            {
+                var updatedEntity = await _uow.GetRepository<Work>().Find(dto.Id);
+                if(updatedEntity != null)
+                {
+                    _uow.GetRepository<Work>().Update(_mapper.Map<Work>(dto),updatedEntity);
+                    await _uow.SaveChanges();
+                    return new Response<WorkUpdateDto>(ResponseType.Success, dto);
+                }
+                return new Response<WorkUpdateDto>(ResponseType.NotFound, $"Data for {dto.Id} not found!");
+            }
+            else
+            {
+                return new Response<WorkUpdateDto>(ResponseType.ValidationError, dto, result.ConvertToCustomValidationError());
+            }
+
+        }
+    }
+}
